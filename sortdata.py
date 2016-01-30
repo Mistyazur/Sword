da3c1dc4a7bf3afcbd863e686c3c7a9f9f3a74e8
Drake Chaos <Drk9Chaos@gmail.com>
2016-01-30 17:14:04 +0800

    None

--- sortdata.py
+++ sortdata.py
@@ -1,6 +1,6 @@
 import os
 
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageEnhance
 
 
 def SortWordText():
@@ -16,16 +16,22 @@ def SortWordText():
 
 
 def SortPic():
-    for root, dirs, files in os.walk(".\\sample"):
+    for root, dirs, files in os.walk(".\\enhance"):
         for name in files:
-            print(os.path.join(root, name))
-            img = Image.open(os.path.join(root, name))
-            img = img.convert("RGB")
-            imgf = img.filter(ImageFilter.SHARPEN)
-            imgf = imgf.filter(ImageFilter.FIND_EDGES)
-            # imgf = imgf.convert("L")
-            # imgf = imgf.point([0 if x < 15 else 1 for x in range(256)], "1")
-            imgf.save(os.path.join(".\\binary", name))
+            if name.find("eh") == -1:
+                print(os.path.join(root, name))
+                img = Image.open(os.path.join(root, name))
+                img = img.convert("RGB")
+
+                # imgf = img.filter(ImageFilter.SHARPEN)
+                # # imgf = imgf.filter(ImageFilter.FIND_EDGES)
+                # # imgf = imgf.convert("L")
+                # # imgf = imgf.point([0 if x < 15 else 1 for x in range(256)], "1")
+
+                enhancer = ImageEnhance.Contrast(img)
+                img = enhancer.enhance(2.5)
+                # img.save(os.path.join(".\\enhance", name.split(".")[0] + "_eh{0}".format(i) + ".bmp"))
+                img.save(os.path.join(".\\enhance", "0eh_" + name))
 
 if __name__ == "__main__":
     # SortData()
